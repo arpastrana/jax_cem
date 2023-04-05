7bfd97899e0b8d252c53a9938e4760ed7701891e
Rafael Pastrana <arpj@princeton.edu>
2023-04-05 09:47:25 -0400

    Off we go

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+import compas
+import jax_cem
+import math
+import numpy
+
+
+def pytest_ignore_collect(path):
+    if "rhino" in str(path):
+        return True
+
+    if "blender" in str(path):
+        return True
+
+    if "ghpython" in str(path):
+        return True
+
+
+@pytest.fixture(autouse=True)
+def add_compas(doctest_namespace):
+    doctest_namespace["compas"] = compas
+
+
+@pytest.fixture(autouse=True)
+def add_jax_cem(doctest_namespace):
+    doctest_namespace["jax_cem"] = jax_cem
+
+
+@pytest.fixture(autouse=True)
+def add_math(doctest_namespace):
+    doctest_namespace["math"] = math
+
+
+@pytest.fixture(autouse=True)
+def add_np(doctest_namespace):
+    doctest_namespace["np"] = numpy
---
+++ docs/index.rst
@@ -0,0 +1,33 @@
+********************************************************************************
+jax_cem
+********************************************************************************
+
+.. rst-class:: lead
+
+The combinatorial equilibrium modeling framework in JAX.
+
+.. .. figure:: /_images/
+     :figclass: figure
+     :class: figure-img img-fluid
+
+
+Table of Contents
+=================
+
+.. toctree::
+   :maxdepth: 3
+   :titlesonly:
+
+   Introduction <self>
+   installation
+   tutorial
+   examples
+   api
+   license
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
