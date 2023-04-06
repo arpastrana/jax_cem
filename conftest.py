fbe302a8fe69b90ab56573c37c670bc74e61b894
Rafael Pastrana <arpj@princeton.edu>
2023-04-06 16:38:42 -0400

    [Tests] Transfered tests from `compas_cem` (no indirect deviation edges)

--- conftest.py
+++ conftest.py
@@ -34,3 +34,264 @@ def add_math(doctest_namespace):
 @pytest.fixture(autouse=True)
 def add_np(doctest_namespace):
     doctest_namespace["np"] = numpy
+
+
+# ==============================================================================
+# Fixtures
+# ==============================================================================
+
+from math import sqrt
+
+import pytest
+
+from compas.geometry import Plane
+
+from compas_cem.diagrams import TopologyDiagram
+from compas_cem.elements import Node
+from compas_cem.elements import TrailEdge
+from compas_cem.elements import DeviationEdge
+from compas_cem.loads import NodeLoad
+from compas_cem.supports import NodeSupport
+
+@pytest.fixture
+def compression_strut():
+    """
+    A one-edge, single-trail compression strut.
+    """
+    topology = TopologyDiagram()
+
+    # add nodes
+    topology.add_node(Node(0, [0.0, 0.0, 0.0]))
+    topology.add_node(Node(1, [0.0, 2.0, 0.0]))
+    # add edge with length 1, in compression
+    topology.add_edge(TrailEdge(0, 1, length=-1.0))
+    # add support
+    topology.add_support(NodeSupport(0))
+    # add loads at the unsupported edge
+    topology.add_load(NodeLoad(1, [0, -1.0, 0.0]))
+
+    return topology
+
+
+@pytest.fixture
+def tension_chain():
+    """
+    A chain with three edges in tension.
+    The lengths of the first and the last edges are implicitly pulled to planes.
+    """
+    topology = TopologyDiagram()
+
+    # add nodes
+    topology.add_node(Node(0, [0.0, 0.0, 0.0]))
+    topology.add_node(Node(1, [1.0, 0.0, 0.0]))
+    topology.add_node(Node(2, [2.0, 0.0, 0.0]))
+    topology.add_node(Node(3, [3.0, 0.0, 0.0]))
+    # add edges
+    topology.add_edge(TrailEdge(0, 1, length=1, plane=([1.5, 0., 0.], [1., 0., 0.])))
+    topology.add_edge(TrailEdge(1, 2, length=1))  # unit length in tension
+    topology.add_edge(TrailEdge(2, 3, length=1, plane=([4.0, 0., 0.], [1., 0., 0.])))
+    # add support
+    topology.add_support(NodeSupport(3))
+    # add load
+    topology.add_load(NodeLoad(0, [-1, 0, 0]))
+
+    return topology
+
+
+@pytest.fixture
+def compression_chain():
+    """
+    A chain with three edges in compression.
+    The lengths of the first and the last edges are implicitly pulled to planes.
+    """
+    topology = TopologyDiagram()
+
+    # add nodes
+    topology.add_node(Node(0, [0.0, 0.0, 0.0]))
+    topology.add_node(Node(1, [1.0, 0.0, 0.0]))
+    topology.add_node(Node(2, [2.0, 0.0, 0.0]))
+    topology.add_node(Node(3, [3.0, 0.0, 0.0]))
+    # add edges
+    topology.add_edge(TrailEdge(0, 1, length=-1, plane=([1.5, 0., 0.], [1., 0., 0.])))
+    topology.add_edge(TrailEdge(1, 2, length=-1))  # unit length in tension
+    topology.add_edge(TrailEdge(2, 3, length=-1, plane=([4.0, 0., 0.], [1., 0., 0.])))
+    # add support
+    topology.add_support(NodeSupport(3))
+    # add load
+    topology.add_load(NodeLoad(0, [1, 0, 0]))
+
+    return topology
+
+
+@pytest.fixture
+def threebar_funicular():
+    """
+    The simplest possible two-trail funicular structure in the CEM.
+    """
+    # create a topology diagram
+    topology = TopologyDiagram()
+
+    # add nodes
+    topology.add_node(Node(0, [0.0, 0.0, 0.0]))
+    topology.add_node(Node(1, [1.0, 0.0, 0.0]))
+    topology.add_node(Node(2, [2.5, 0.0, 0.0]))
+    topology.add_node(Node(3, [3.5, 0.0, 0.0]))
+
+    # add edges with negative values for a compression-only structure
+    topology.add_edge(TrailEdge(0, 1, length=-1.0))
+    topology.add_edge(DeviationEdge(1, 2, force=-1.0))
+    topology.add_edge(TrailEdge(2, 3, length=-1.0))
+
+    # add supports
+    topology.add_support(NodeSupport(0))
+    topology.add_support(NodeSupport(3))
+
+    # add loads
+    topology.add_load(NodeLoad(1, [0.0, -1.0, 0.0]))
+    topology.add_load(NodeLoad(2, [0.0, -1.0, 0.0]))
+
+    return topology
+
+
+@pytest.fixture
+def braced_tower_2d():
+    """
+    A braced tower in 2d.
+    """
+    points = [(0, [0.0, 0.0, 0.0]),
+              (1, [0.0, 1.0, 0.0]),
+              (2, [0.0, 2.0, 0.0]),
+              (3, [1.0, 0.0, 0.0]),
+              (4, [1.0, 1.0, 0.0]),
+              (5, [1.0, 2.0, 0.0])]
+
+    trail_edges = [(0, 1),
+                   (1, 2),
+                   (3, 4),
+                   (4, 5)]
+
+    deviation_edges = [(1, 4),
+                       (2, 5)]
+
+    load = [0.0, -1.0, 0.0]
+
+    topology = TopologyDiagram()
+
+    for key, point in points:
+        topology.add_node(Node(key, point))
+
+    for u, v in trail_edges:
+        topology.add_edge(TrailEdge(u, v, length=-1.0))
+
+    for u, v in deviation_edges:
+        topology.add_edge(DeviationEdge(u, v, force=-1.0))
+
+    topology.add_edge(DeviationEdge(1, 5, force=1.0))
+    topology.add_edge(DeviationEdge(1, 3, force=1.0))
+    topology.add_edge(DeviationEdge(2, 4, force=1.0))
+
+    topology.add_support(NodeSupport(0))
+    topology.add_support(NodeSupport(3))
+
+    topology.add_load(NodeLoad(2, load))
+    topology.add_load(NodeLoad(5, load))
+
+    return topology
+
+@pytest.fixture
+def tree_2d_needs_auxiliary_trails():
+    """
+    An planar tree that is missing two auxiliary trails to be valid topologically.
+    """
+    width = 4
+    height = width / 2
+
+    # Topology diagram
+    topology = TopologyDiagram()
+
+    # add nodes
+    topology.add_node(Node(1, [-width / 2, height, 0.0]))
+    topology.add_node(Node(2, [width / 2, height, 0.0]))
+    topology.add_node(Node(3, [0.0, height / 2, 0.0]))
+    topology.add_node(Node(4, [0.0, 0.0, 0.0]))
+
+    # add edges with negative values for a compression-only structure
+    topology.add_edge(TrailEdge(3, 4, length=-height/2))
+
+    topology.add_edge(DeviationEdge(1, 3, force=-sqrt(4.0)))
+    topology.add_edge(DeviationEdge(2, 3, force=-sqrt(2.0)))
+    topology.add_edge(DeviationEdge(1, 2, force=2.0))
+
+    # add supports
+    topology.add_support(NodeSupport(4))
+
+    # add loads
+    topology.add_load(NodeLoad(1, [0.0, -1.0, 0.0]))
+    topology.add_load(NodeLoad(2, [0.0, -1.0, 0.0]))
+
+    return topology
+
+@pytest.fixture
+def support_missing_topology():
+    """
+    A topology with three edges supposed to topology two trails. One support is missing.
+    """
+    topology = TopologyDiagram()
+    # add five nodes
+    for node_key in range(5):
+        topology.add_node(Node(node_key, xyz=[0.0, float(node_key), 0.0]))
+
+    # add two trail edges and one weird deviation edge
+    topology.add_edge(TrailEdge(0, 1, length=1))
+    topology.add_edge(TrailEdge(1, 2, length=1))
+    topology.add_edge(DeviationEdge(3, 4, force=1))
+
+    # add load
+    topology.add_load(NodeLoad(0, [0, -1.0, 0.0]))
+
+    # add only one support
+    topology.add_support(NodeSupport(2))
+
+    return topology
+
+
+@pytest.fixture
+def no_trails_topology():
+    """
+    A topology with only two deviation edges.
+    """
+    topology = TopologyDiagram()
+    # add five nodes
+    for node_key in range(3):
+        topology.add_node(Node(node_key, xyz=[0.0, float(node_key), 0.0]))
+
+    # add two trail edges and one weird deviation edge
+    topology.add_edge(DeviationEdge(0, 1, force=1))
+    topology.add_edge(DeviationEdge(1, 2, force=1))
+
+    # add load
+    topology.add_load(NodeLoad(0, [0, -1.0, 0.0]))
+
+    # add only one support
+    topology.add_support(NodeSupport(2))
+
+    return topology
+
+
+@pytest.fixture
+def unsupported_topology():
+    """
+    A topology with one trail edge and a node load, but no supports.
+    """
+    topology = TopologyDiagram()
+    # add five nodes
+    for node_key in range(2):
+        topology.add_node(Node(node_key, xyz=[0.0, float(node_key), 0.0]))
+
+    # add two trail edges and one weird deviation edge
+    topology.add_edge(TrailEdge(0, 1, length=-1))
+
+    # add load
+    topology.add_load(NodeLoad(0, [0, -1.0, 0.0]))
+
+    return topology
---
+++ tests/test_equilibrium.py
@@ -0,0 +1,184 @@
+
+import pytest
+
+import numpy as np
+
+from jax_cem.equilibrium import EquilibriumModel
+from jax_cem.equilibrium import EquilibriumStructure
+from jax_cem.equilibrium import form_from_eqstate
+
+
+# ==============================================================================
+# Tests - Static equilibrium
+# ==============================================================================
+
+def cs_out():
+    """Static equilibrium results from compression strut."""
+    output = {}
+    output["xyz"] = {0: [0.0, 1.0, 0.0], 1: [0.0, 2.0, 0.0]}
+    output["force"] = {(0, 1): -1.0}
+    output["length"] = {(0, 1): 1.0}
+    output["residual"] = {0: [0.0, 1.0, 0.0]}
+
+    return output
+
+
+def tc_out():
+    """Static equilibrium results from tension chain."""
+    output = {}
+    output["xyz"] = {0: [0.0, 0.0, 0.0],
+                     1: [1.5, 0.0, 0.0],
+                     2: [2.5, 0.0, 0.0],
+                     3: [4.0, 0.0, 0.0]}
+
+    output["force"] = {(0, 1): 1.0,
+                       (1, 2): 1.0,
+                       (2, 3): 1.0}
+
+    output["length"] = {(0, 1): 1.5,
+                        (1, 2): 1.0,
+                        (2, 3): 1.5}
+
+    output["residual"] = {3: [1.0, 0.0, 0.0]}
+
+    return output
+
+
+def cc_out():
+    """Static equilibrium results from compression chain."""
+    output = {}
+    output["xyz"] = {0: [0.0, 0.0, 0.0],
+                     1: [1.5, 0.0, 0.0],
+                     2: [2.5, 0.0, 0.0],
+                     3: [4.0, 0.0, 0.0]}
+
+    output["force"] = {(0, 1): -1.0,
+                       (1, 2): -1.0,
+                       (2, 3): -1.0}
+
+    output["length"] = {(0, 1): 1.5,
+                        (1, 2): 1.0,
+                        (2, 3): 1.5}
+
+    output["residual"] = {3: [-1.0, 0.0, 0.0]}
+
+    return output
+
+
+def tf_out():
+    """Static equilibrium results from threebar funicular."""
+    output = {}
+    output["xyz"] = {0: [0.29289321881345254, -0.7071067811865475, 0.0],
+                     1: [1.0, 0.0, 0.0],
+                     2: [2.5, 0.0, 0.0],
+                     3: [3.207106, -0.7071067, 0.0]}
+
+    output["force"] = {(0, 1): -1.414213561,
+                       (1, 2): -1.0,
+                       (2, 3): -1.41421356}
+
+    output["length"] = {(0, 1): 1.0,
+                        (1, 2): 1.5,
+                        (2, 3): 1.0}
+
+    output["residual"] = {0: [1.0, 1.0, -0.0],
+                          3: [-1.0, 1.0, 0.0]}
+
+    return output
+
+
+def bt2_out():
+    """Static equilibrium results from braced tower 2d."""
+    output = {}
+    output["xyz"] = {0: [0.11891271935545733, 0.04623304043571308, 0.0],
+                     1: [-0.14550216351451895, 1.0106420665842952, 0.0],
+                     2: [0.0, 2.0, 0.0],
+                     3: [1.5829003695589805, 0.11137412111377487, 0.0],
+                     4: [1.1455022100524879, 1.010642073428508, 0.0],
+                     5: [1.0, 2.0, 0.0]}
+
+    output["force"] = {(0, 1): -1.5154917766302523,
+                       (1, 2): -1.6714301665432025,
+                       (1, 4): -1.0,
+                       (1, 5): 1.0,
+                       (1, 3): 1.0,
+                       (2, 5): -1.0,
+                       (2, 4): 1.0,
+                       (3, 4): -1.1120156232900127,
+                       (4, 5): -1.6714302901903129}
+
+    output["length"] = {(0, 1): 1.0,
+                        (1, 2): 1.0,
+                        (1, 4): 1.2910043735670067,
+                        (1, 5): 1.5136063976572767,
+                        (1, 3): 1.9483475444811331,
+                        (2, 5): 1.0,
+                        (2, 4): 1.5136064284036903,
+                        (3, 4): 1.0,
+                        (4, 5): 1.0}
+
+    output["residual"] = {0: [-0.4007185806081004, 1.4615539484361662, -0.0],
+                          3: [0.40071844900288345, 0.5384458270605735, -0.0]}
+
+    return output
+
+
+@pytest.mark.parametrize("topology, output",
+                         [(pytest.lazy_fixture("compression_strut"), cs_out()),
+                          (pytest.lazy_fixture("threebar_funicular"), tf_out()),
+                          # (pytest.lazy_fixture("braced_tower_2d"), bt2_out()),
+                          (pytest.lazy_fixture("tension_chain"), tc_out()),
+                          (pytest.lazy_fixture("compression_chain"), cc_out())
+                          ])
+def test_force_equilibrium_jax_output(topology, output):
+    """
+    Minute testing of forces and geometric outputs post force equilibrium.
+    """
+    node_xyz_out = output["xyz"]
+    edge_force_out = output["force"]
+    edge_length_out = output["length"]
+    support_residual_out = output["residual"]
+
+    topology.build_trails()
+
+    structure = EquilibriumStructure.from_topology_diagram(topology)
+    model = EquilibriumModel.from_topology_diagram(topology)
+    eqstate = model(structure)
+    form = form_from_eqstate(structure, eqstate)
+
+    check_nodes_xyz(form, node_xyz_out)
+    check_edges_forces(form, edge_force_out)
+    check_edges_lengths(form, edge_length_out)
+    check_nodes_reactions(form, support_residual_out)
+
+
+# ==============================================================================
+# Tests - Force Equilibrium Queries
+# ==============================================================================
+
+def check_nodes_xyz(form, node_xyz_out):
+    for node in form.nodes(data=False):
+        xyz = node_xyz_out.get(node)
+        test_xyz = form.node_coordinates(node)
+        assert np.allclose(xyz, test_xyz)
+
+
+def check_edges_forces(form, edge_force_out):
+    for edge in form.edges(data=False):
+        force = edge_force_out.get(edge)
+        test_force = form.edge_force(edge)
+        assert np.allclose(force, test_force)
+
+
+def check_edges_lengths(form, edge_length_out):
+    for edge in form.edges(data=False):
+        length = edge_length_out.get(edge)
+        test_length = form.edge_length(*edge)  # TODO: overwrite inheritance
+        assert np.allclose(length, test_length)
+
+
+def check_nodes_reactions(form, support_residual_out):
+    for node in form.nodes(data=False):
+        residual = support_residual_out.get(node, [0.0, 0.0, 0.0])
+        test_residual = form.reaction_force(node)
+        assert np.allclose(residual, test_residual)
